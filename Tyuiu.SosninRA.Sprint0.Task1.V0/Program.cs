9dccb08bec7e2eaf9cc39eb9c649c2d9ecfd6774
Руслан <@WIN-IKIRV68J14N>
2026-09-10 20:43:45 +0500

    Добавьте файлы проекта.

---
+++ Tyuiu.SosninRA.Sprint0.Task1.V0/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Tyuiu.SosninRA.Sprint0.Task1.V0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
