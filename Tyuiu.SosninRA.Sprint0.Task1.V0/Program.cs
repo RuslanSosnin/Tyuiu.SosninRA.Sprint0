d35117d2ca18d56b8d8f3467250bd4d9de07de71
Ruslan <flowrascal@gmail.com>
2026-09-13 13:40:00 +0500

    Task1

---
+++ Tyuiu.SosninRA.Sprint0.Task1.V0/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Tyuiu.SosninRA.Sprint0.Task1.V0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            string name = "Руслан";
+            int age = 19;
+            Console.WriteLine("Здравствуйте, меня зовут " + name + ", мне " + age + " лет!");
+            Console.ReadKey();
+        }
+    }
+}
