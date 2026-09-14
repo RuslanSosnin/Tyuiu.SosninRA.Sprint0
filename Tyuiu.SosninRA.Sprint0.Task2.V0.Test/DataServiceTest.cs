3ef86dcdd974094a9a8b0d4ba4e3ff81f202aa86
Ruslan <flowrascal@gmail.com>
2026-09-14 15:02:54 +0500

    Task2

---
+++ Tyuiu.SosninRA.Sprint0.Task2.V0.Test/DataServiceTest.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using Tyuiu.SosninRA.Sprint0.Task2.V0.Lib;
+
+namespace Tyuiu.SosninRA.Sprint0.Task2.V0.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ChekGetMessageValid()
+        {
+            var name = "Руслан";
+            var res = DataService.GetMessage(name);
+
+            Assert.AreEqual("Привет, Руслан", res);
+        }
+    }
+}
