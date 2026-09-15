9b42460356cf855698edbc9bc25dc03220296ba6
Ruslan <flowrascal@gmail.com>
2026-09-15 20:34:36 +0500

    Task5

---
+++ Tyuiu.SosninRA.Sprint0.Task5.V0.Test/DataServiceTest.cs
@@ -0,0 +1,29 @@
+﻿using Tyuiu.SosninRA.Sprint0.Task5.V0.Lib;
+
+namespace Tyuiu.SosninRA.Sprint0.Task5.V0.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void CheckAdditionValid()
+        {
+            Assert.AreEqual(10, DataService.Addition(5, 5));
+        }
+        [TestMethod]
+        public void CheckedSubtractionValid()
+        {
+            Assert.AreEqual(5, DataService.Subtraction(10, 5));
+        }
+        [TestMethod]
+        public void CheckedMultiplicationValid()
+        {
+            Assert.AreEqual(50, DataService.Multiplication(10, 5));
+        }
+        [TestMethod]
+        public void CheckedDivisionValid()
+        {
+            Assert.AreEqual(3, DataService.Division(9, 3));
+        }
+    }
+}
